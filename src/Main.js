d0f2f751f46cf81c98cd451615fec4b114a2d5e4
이주영 <shm03088@douzone.com>
2018-12-19 15:49:27 +0900

    timeline소스 옮기는거까지 해결

--- src/Main.js
+++ src/Main.js
@@ -1,36 +1,42 @@
 import React from "react";
-import Reduce from './Reduce';
-import ExamplePromise from './ExamplePromise';
-import ExampleIterator from './ExampleIterator';
-import ExampleAsyncAwait from './ExampleAsyncAwait';
-import ExampleAsync2 from "./ExampleAsync2";
-import ExampleCloser from "./ExampleCloser";
-import ExampleFinally from "./ExampleFinally";
-// import "@babel/polyfill"; //genterator사용하려고 임포트함  https://stackoverflow.com/questions/49253746/error-regeneratorruntime-is-not-defined-with-babel-7
-                          //https://esausilva.com/2017/07/11/uncaught-referenceerror-regeneratorruntime-is-not-defined-two-solutions/
-//위에꺼 다 필요 없음 https://www.zerocho.com/category/ECMAScript/post/57a830cfa1d6971500059d5a
-// import Generator from './Generator';
-// import AsyncAwait from './AsyncAwait';
-import ExampleCodeSplitting from './ExampleCodeSplitting';
-import ExamplePortal from './ExamplePortal';
-import ExampleState from "./ExampleState";
+import '../timeline3/js/timeline';
 
 class Main extends React.Component {
-    render() { //  return 배열은 가능, 각각의 독립적인 element는 감싸야함
-               //https://pawelgrzybek.com/return-multiple-elements-from-a-component-with-react-16/
+    render() {
+        // var additionalOptions = {
+        //     start_at_end: true,
+        //     default_bg_color: {r:0, g:0, b:0},
+        //     timenav_height: 250
+        // }
+
+        // const timeline = new TL.Timeline('timeline-embed',
+        //     'https://docs.google.com/spreadsheets/d/1cWqQBZCkX9GpzFtxCWHoqFXCHg-ylTVUWlnrdYMzKUI/pubhtml', additionalOptions)
+        //사용방법: https://timeline.knightlab.com/docs/instantiate-a-timeline.html
+        // option: https://timeline.knightlab.com/docs/options.html
+
+/////--------------------------------------------------------------------------------------------------------------------------------//////////////////////
+//         var lang = 'https://docs.google.com/spreadsheets/d/1cWqQBZCkX9GpzFtxCWHoqFXCHg-ylTVUWlnrdYMzKUI/pubhtml'.match(/&lang=([a-zA-Z]*)&?/);
+//
+//         if (lang) {
+//             document.getElementsByTagName('html')[0].setAttribute('lang', lang[1]);
+//         }
+//         var trim_point = window.location.href.indexOf('embed/index.html');
+//         if (trim_point > 0) {
+//             var embed_path = window.location.href.substring(0,trim_point); // supports https access via https://s3.amazonaws.com/cdn.knightlab.com/libs/timeline/latest/embed/index.html
+//         } else {
+//             var embed_path = "https://cdn.knightlab.com/libs/timeline3/latest/";
+//         }
+//         var oembed_link = document.createElement('link');
+//         oembed_link['rel'] = 'alternate';
+//         oembed_link['type'] = 'application/json+oembed';
+//         oembed_link['href'] = 'https://oembed.knightlab.com/timeline/?url=' + encodeURIComponent(window.location.href);
+//         document.head.appendChild(oembed_link);
+
+        const timeline = new TL.Timeline('timeline-embed', 'https://docs.google.com/spreadsheets/d/1cWqQBZCkX9GpzFtxCWHoqFXCHg-ylTVUWlnrdYMzKUI/pubhtml');
         return (
-            <React.Fragment>
-                {/*<Reduce/>*/}
-                {/*<ExamplePromise/>*/}
-                {/*<ExampleIterator/>*/}
-                {/*<ExampleAsyncAwait/>*/}
-                {/*<ExampleAsync2/>*/}
-                {/*<ExampleCloser/>*/}
-                {/*<ExampleFinally/>*/}
-                {/*<ExampleCodeSplitting/>*/}
-                {/*<ExamplePortal/>*/}
-                <ExampleState/>
-            </React.Fragment>
+            <div>
+                <timeline/>
+            </div>
         )
     }
 
--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ import Main from './Main';
 
 ReactDOM.render(
     <Main/>,
-    document.getElementById("root")
+    document.getElementById("timeline-embed")
 );
